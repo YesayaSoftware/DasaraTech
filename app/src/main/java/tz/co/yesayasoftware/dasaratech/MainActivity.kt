8fb8afc3b15c73193a726dc375a40fc1efb44506
Yesaya R. Athuman <yesayaathuman@gmail.com>
2021-12-09 08:25:00 +0300

    Iniitial commit

---
+++ app/src/main/java/tz/co/yesayasoftware/dasaratech/MainActivity.kt
@@ -0,0 +1,13 @@
+package tz.co.yesayasoftware.dasaratech
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent { }
+    }
+}
